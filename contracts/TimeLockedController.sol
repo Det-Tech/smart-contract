a67218e6ad189925d6a235098e5ae795c5835bfb
proninkirill <proninkirilll228@gmail.com>
2018-10-15 10:51:27 -0700

    add contracts/deployed

--- contracts/TimeLockedController.sol
+++ contracts/TimeLockedController.sol
@@ -314,12 +314,8 @@ contract TimeLockedController is HasRegistry, HasNoEther, HasNoTokens, Claimable
     /** 
     *@dev return current time in pacific time
     */
-    function returnTime() public view returns (uint256) {
+    function returnTime() public view returns(uint256) {
         return now.sub(timeZoneDiff);
-    }
-
-    function mintOperationCount() public view returns (uint256) {
-        return mintOperations.length;
     }
 
     /*
