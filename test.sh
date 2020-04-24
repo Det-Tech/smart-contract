4485710816fb1b326cea34af75ec6a1cd8e12a0a
proninkirill <proninkirilll228@gmail.com>
2020-04-24 16:17:30 +0300

    Improve tests

--- test.sh
+++ test.sh
@@ -4,10 +4,10 @@ set -e
 
 cd $(dirname $0)
 
-npx ganache-cli -l 0x8954400 --allowUnlimitedContractSize -k istanbul >/dev/null &
+ganache-cli -l 0x8954400 --allowUnlimitedContractSize -k istanbul >/dev/null &
 GPID=$!
 sleep 1
-npx truffle test $@
+truffle test $@
 kill -15 $GPID
 
 exit
